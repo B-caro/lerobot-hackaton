c88290b48aeac435706d18def9fc2a66a31a42c7
RicardoFv2 <fuentesrf0@gmail.com>
2025-06-15 13:56:49 -0600

    Agregar copiar JSON de metadatos del dataset y botón para mostrar el JSON completo

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,20 @@ module.exports = {
   },
   plugins: [],
   darkMode: 'class',
-} 
+
+  safelist: [
+    'text-green-700',
+    'dark:text-green-400',
+    'text-blue-700',
+    'dark:text-blue-400',
+    'text-yellow-600',
+    'dark:text-yellow-400',
+    'text-pink-600',
+    'dark:text-pink-400',
+    'text-purple-700',
+    'dark:text-purple-400',
+    'text-gray-500',
+    'italic',
+    'dark:text-gray-400',
+  ],
+};
